a372f80dbd7d9afb07501f277deb4cb2ec8781c4
gurunathan <guru@gegosoft.com>
2026-06-01 13:50:46 +0530

    Changes feedback request

--- app/Http/Requests/FeedbackRequest.php
+++ app/Http/Requests/FeedbackRequest.php
@@ -24,39 +24,38 @@ class FeedbackRequest extends FormRequest
      */
     public function rules()
     {
-        Validator::extend('check_message',function($attribute,$value,$parameters,$validator)
-        {
-            return preg_match('/^[A-Za-z0-9_~\-!@#\$%\^&*.,:(\)\s]+$/', request('message')) ;
+        Validator::extend('check_message', function ($attribute, $value, $parameters, $validator) {
+            return preg_match('/^[A-Za-z0-9_~\-!@#\$%\^&*.,:(\)\s]+$/', request('message'));
         });
 
-        Validator::extend('check_count',function($attribute,$value,$parameters,$validator) 
-        {
+        Validator::extend('check_count', function ($attribute, $value, $parameters, $validator) {
             $file_count = count(request('files'));
-            if($file_count > 6)
-            {
+            if ($file_count > 6) {
                 return false;
             }
             return true;
         });
 
-        Validator::extend('check_file_extension', function ($attribute, $value, $parameters, $validator)
-        {
-            $extension=$value->getClientOriginalExtension();
+        Validator::extend('check_file_extension', function ($attribute, $value, $parameters, $validator) {
+            $extension = $value->getClientOriginalExtension();
             return $extension != '' && in_array($extension, $parameters);
         });
 
+        // $rules = [
+        //     'message'   =>  'required|max:300|check_message',
+        //     'category'  =>  'required',//|check_count
+        // ];
+
         $rules = [
-            //
-            'message'   =>  'required|max:300|check_message',
-            'category'  =>  'required',//|check_count
+            'message'  => 'required|max:300|check_message',
+            'category' => 'required|in:bug,suggestion,others',
         ];
 
         $files = request('files');
         $rules['files.*'] = 'check_file_extension:jpeg,jpg,png|max:2048';
-        
+
         return $rules;
     }
-
     public function messages()
     {
         return [
@@ -66,6 +65,7 @@ class FeedbackRequest extends FormRequest
             'message.check_message' =>  'Enter Valid Message',
 
             'category.required'     =>  'Category Is Required',
+            'category.in'           => 'Please select a valid category',
 
             'category.check_count'  =>  'Attachment cannot be more than 6',
             'files.max'             =>  'Attachment size should be within 2MB',
