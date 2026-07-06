e3407f3be5afdafe09af40085252cc30189bc3c2
gurunathan <guru@gegosoft.com>
2026-07-06 20:55:13 +0530

    Added qr code in footer

--- resources/webbuilder/themes/default/_footer.blade.php
+++ resources/webbuilder/themes/default/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="bg-gray-900 text-gray-400 mt-16 pt-4">
 
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-10">
 
             {{-- Col 1: Brand --}}
             <div class="lg:col-span-1">
